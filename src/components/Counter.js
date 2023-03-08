b60c51e06548bae662cde083cc08c0852c1581f7
davi <davigabtomaz@gmail.com>
2023-03-08 11:45:54 -0300

    Feito Text Input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,38 @@
 import { Component } from 'react';
 import { Button, Text, View, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default class Counter extends Component {
     state = {
         count: 0,
+        step: 2,
     };
-    incrementar(){
-        this.setState({ count: this.state.count + 1});
+    incrementar() {
+        this.setState({ count: this.state.count + this.state.step });
     }
-    decrementar(){
-        this.setState({ count: this.state.count - 1});
+    decrementar() {
+        this.setState({ count: this.state.count - 1 });
+    }
+    alteraStep(e) {
+        this.setState({ step: parseInt(e.nativeEvent.text) })
+    }
+    alteraStep2(text) {
+        this.setState({ step: parseInt(text) })
     }
     render() {
         return (
-            <View style={StyleSheet.contador}>
-            <Text>Contador: {this.state.count}</Text>
-            <Button onPress={() => this.incrementar()} title='+'/>
-            <Button onPress={() => this.decrementar()} title='-'/>
+            <View style={styles.contador}>
+                <TextInput value={this.state.step}
+                 onChange={(e) => this.alteraStep(e)}
+                 inputMode="numeric"
+                 keyboardType='numeric' />
+                 <TextInput value={this.state.step}
+                 onChangeText={(text) => this.alteraStep2(text)}
+                 inputMode="numeric"
+                 keyboardType='numeric' />
+                <Text>Contador: {this.state.count} - {this.state.step}</Text>
+                <Button style={styles.botao} onPress={() => this.incrementar()} title='+'  />
+                <Button onPress={() => this.decrementar()} title='-' />
             </View>
         );
     }
@@ -26,4 +42,5 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alingItems: 'center',
     },
-});
+   
+);
